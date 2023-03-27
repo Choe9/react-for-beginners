21ca1124928d81e66b26aa6e7f38f7c50e9dd410
DESKTOP-NSNJVU7\runcj <run1529@gmail.com>
2023-03-27 18:14:28 +0900

    init commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/abot-us" element={ <h1>Hello</h1> } >
+        </Route>
+        <Route path="/movie/:id" element={ <Detail /> } >
+        </Route>
+        <Route path="/" element={ <Home /> } >
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
